6124bd1c1d5a4bd67d506be0313f8dd2dae4177c
Vitalii Prozapas <vitalii.prozapas@gmail.com>
2022-12-26 02:26:42 +0100

    Task 3 done, add some info to readme.md too

--- src/main/java/io/allteran/codetask2022/controller/Task3Controller.java
+++ src/main/java/io/allteran/codetask2022/controller/Task3Controller.java
@@ -1,5 +1,8 @@
 package io.allteran.codetask2022.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.allteran.codetask2022.dto.Task3DTO;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 @RestController
 @RequestMapping("task3")
 public class Task3Controller {
-    @Value("${uri.okg-se}")
+    @Value("${uri.task3}")
     private String URI_TASK3;
     private final WebClient.Builder webClientBuilder;
 
@@ -19,24 +28,24 @@ public class Task3Controller {
         this.webClientBuilder = webClientBuilder;
     }
 
-    @GetMapping("get-info")
+    @SneakyThrows
+    @GetMapping("print-info")
     public ResponseEntity<String> getAndPrintO3() {
+        String response = webClientBuilder.build().get()
+                //thanks to reverse engineering and God I found exactly that URL that gives actual info about O3 level in JSON
+                //it was stored in component---src-templates-startpage-container-js-cb21106a3721eccdf9d8.js
+                .uri(URI_TASK3)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
 
-//        String response = webClientBuilder.build().get()
-//                .uri(URI_TASK3)
-//                .header("Content-Type", "text/html; charset=UTF-8")
-//                .retrieve()
-//                .bodyToMono(String.class)
-//                .block();
+        Task3DTO output = new ObjectMapper().readValue(response, Task3DTO.class);
+        LocalDateTime time = LocalDateTime.parse(output.getTime().substring(0, output.getTime().lastIndexOf("+1")).replace(" ", ""));
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String timeString = time.format(dtf);
 
-//        String response = webClientBuilder.filter((request, next) -> next.exchange(request)
-//                .map(r -> {
-//                    ResponseDecorator decorated = new ResponseDecorator(r);
-//                }))
+        System.out.println("value:" + output.getOutput() + ", time:" + timeString);
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String > response = restTemplate.getForEntity(URI_TASK3, String.class);
-
-        return response;
+        return ResponseEntity.ok("SUCCESS");
     }
 }
---
+++ src/main/java/io/allteran/codetask2022/dto/Task3DTO.java
@@ -0,0 +1,13 @@
+package io.allteran.codetask2022.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Task3DTO {
+    private String output;
+    private String time;
+}
